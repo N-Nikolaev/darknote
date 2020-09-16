f69c805696e244b03647de51ccfa2e76c154929f
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2020-09-16 13:05:33 +0800

    feat(notepad-view): added notepad-view sub-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NotepadComponent } from './notepad/notepad.component';
+import { NotepadViewComponent } from './notepad/notepad-view/notepad-view.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, NotepadComponent, NotepadViewComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/notepad/notepad-view/notepad-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notepad-view',
+  templateUrl: './notepad-view.component.html',
+  styleUrls: ['./notepad-view.component.sass']
+})
+export class NotepadViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
