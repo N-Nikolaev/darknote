3b2fd5c61aae977162fd3f884594d814c041b614
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2020-10-03 12:57:08 +0800

    feat(notepad-list): created the notepad-list component
    Used ng-cli to create a component named "notepad-list" as well as update the app.module

---
+++ src/app/notepad/notepad-list/notepad-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notepad-list',
+  templateUrl: './notepad-list.component.html',
+  styleUrls: ['./notepad-list.component.sass']
+})
+export class NotepadListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
