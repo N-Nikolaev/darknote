51e16c957df75c126ff17758b56dc41757272dbc
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2020-10-03 13:16:44 +0800

    feat(notepad-list): added component HTML

--- src/app/notepad/notepad-list/notepad-list.component.ts
+++ src/app/notepad/notepad-list/notepad-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../../shared/note.model';
 
 @Component({
   selector: 'app-notepad-list',
   templateUrl: './notepad-list.component.html',
-  styleUrls: ['./notepad-list.component.sass']
+  styleUrls: ['./notepad-list.component.sass'],
 })
 export class NotepadListComponent implements OnInit {
+  notes: Note[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSelected(note: Note): void {}
+
+  onDelete(id: string): void {}
+
+  ngOnDestroy(): void {}
 }
