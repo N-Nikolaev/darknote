0a253cb7e32a5e9f34b3e18b173fdc940f26d938
Nikolay Nikolaev <nikolaynikolaev.dev@gmail.com>
2020-10-03 13:59:24 +0800

    chore(note.service): removed unused imports

--- src/app/notepad/note.service.ts
+++ src/app/notepad/note.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Subject } from 'rxjs';
